cc4e7b5385f7a89d665eadf6c62aa03ac7a18a1e
akane.senseki <ajajggpw@gmail.com>
2020-12-03 16:52:39 +0900

    2020.12.03updateServlet

---
+++ WebContent/WEB-INF/views/employees/edit.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:choose>
+            <c:when test="${employee != null}">
+                <h2>id:</h2>
+            </c:when>
+        </c:choose>
+    </c:param>
+</c:import>
--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -1,19 +1,26 @@
 package controllers.employees;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
 /**
  * Servlet implementation class EmployeesUpdateServlet
  */
 @WebServlet("/employees/update")
 public class EmployeesUpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +29,44 @@ public class EmployeesUpdateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String _token = (String) request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Employee e = em.find(Employee.class, (Integer) (request.getSession().getAttribute("employee_id")));
+
+            //現在の値と異なる社員番号が入力されていたら重複チェックを行う指定をする
+            Boolean codeDuplicateCheckFlag = true;
+            if (e.getCode().equals(request.getParameter("code"))) {
+                codeDuplicateCheckFlag = false;
+            } else {
+                e.setCode(request.getParameter("code"));
+            }
+            //パスワード欄に入力があったら
+            //入力値チェックを行う指定をする
+            Boolean passwordCheckFlag = true;
+            String password = request.getParameter("password");
+            if (password == null || password.equals("")) {
+                passwordCheckFlag = false;
+            } else {
+                e.setPassword(
+                        EncryptUtil.getPasswordEncrypt(
+                                password,
+                                (String) this.getServletContext().getAttribute("pepper")
+                                )
+                        );
+            }
+            e.setName(request.getParameter("name"));
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
+            e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+            e.setDelete_flag(0);
+
+        }
+    }
 
 }
